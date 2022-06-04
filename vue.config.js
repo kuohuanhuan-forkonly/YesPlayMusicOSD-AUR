dda77ace44d3187eed71237cd9af0f084def035a
郭桓桓 <hi@nekohuan.cyou>
2022-06-04 15:43:10 +0800

    fix(build): err buildin' AUR pak - try#3

--- vue.config.js
+++ vue.config.js
@@ -127,13 +127,9 @@ module.exports = {
               arch: ['x64'],
             },
             {
-              target: 'tar.gz',
-              arch: ['x64', 'arm64'],
-            },
-            {
               target: 'pacman',
               arch: ['x64'],
-            },
+            }
           ],
           category: 'Music',
           icon: './build/icon.icns',
